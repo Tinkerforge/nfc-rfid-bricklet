f073e093ee9207a690c57a39fbc2c46f5373f42f
Olaf Lüke <olaf@tinkerforge.com>
2014-05-30 13:38:25 +0200

    Add examples for C#

---
+++ software/examples/csharp/ExampleScanForTags.cs
@@ -0,0 +1,57 @@
+using Tinkerforge;
+
+class Example
+{
+	private static string HOST = "localhost";
+	private static int PORT = 4223;
+	private static string UID = "hjw"; // Change to your UID
+
+	private static byte tagType = 0;
+
+	// Callback function for state changed callback
+	static void StateChangedCB(BrickletNFCRFID sender, byte state, bool idle)
+	{
+		if(idle)
+		{
+			tagType = (byte)((tagType + 1) % 3);
+			sender.RequestTagID(tagType);
+		}
+
+		if(state == BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY)
+		{
+			byte newTagType;
+			byte tidLength;
+			byte[] tid;
+			sender.GetTagID(out newTagType, out tidLength, out tid);
+
+			if(tidLength == 7)
+			{
+				System.Console.WriteLine("Fount tag of type {0} with ID [{1:X}, {2:X}, {3:X}, {4:X}, {5:X}, {6:X}, {7:X}]",
+										 newTagType, tid[0], tid[1], tid[2], tid[3], tid[4], tid[5], tid[6]);
+			}
+			else
+			{
+				System.Console.WriteLine("Fount tag of type {0} with ID [{1:X}, {2:X}, {3:X}, {4:X}]",
+										 newTagType, tid[0], tid[1], tid[2], tid[3]);
+			}
+		}
+	}
+
+	static void Main() 
+	{
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletNFCRFID nfc = new BrickletNFCRFID(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
+
+		// Register state changed callback to function StateChangedCB
+		nfc.StateChanged += StateChangedCB;
+
+		nfc.RequestTagID(BrickletNFCRFID.TAG_TYPE_MIFARE_CLASSIC);
+
+		System.Console.WriteLine("Press key to exit");
+		System.Console.ReadKey();
+		ipcon.Disconnect();
+    }
+}
---
+++ software/examples/csharp/ExampleWriteReadType2.cs
@@ -0,0 +1,63 @@
+using Tinkerforge;
+
+class Example
+{
+	private static string HOST = "localhost";
+	private static int PORT = 4223;
+	private static string UID = "hjw"; // Change to your UID
+
+	// Callback function for state changed callback
+	static void StateChangedCB(BrickletNFCRFID sender, byte state, bool idle)
+	{
+		if(state == BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY)
+		{
+			System.Console.WriteLine("Tag found");
+
+			// Write 16 byte to pages 5-8
+			byte[] dataWrite = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+			sender.WritePage(5, dataWrite);
+			System.Console.WriteLine("Writing data...");
+		}
+		else if(state == BrickletNFCRFID.STATE_WRITE_PAGE_READY)
+		{
+			// Request pages 5-8
+			sender.RequestPage(5);
+			System.Console.WriteLine("Requesting data...");
+		}
+		else if(state == BrickletNFCRFID.STATE_REQUEST_PAGE_READY)
+		{
+			// Get and print pages
+			byte[] data = sender.GetPage();
+			string str = "" + data[0];
+			for(int i = 1; i < data.Length; i++)
+			{
+				str += ", " + data[i];
+			}
+			System.Console.Write("Read data: [" + str + "]");
+		}
+		else if((state & (1 << 6)) == (1 << 6))
+		{
+			// All errors have bit 6 set
+			System.Console.WriteLine("Error: " + state);
+		}
+	}
+
+	static void Main() 
+	{
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletNFCRFID nfc = new BrickletNFCRFID(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
+
+		// Register state changed callback to function StateChangedCB
+		nfc.StateChanged += StateChangedCB;
+
+		// Select NFC Forum Type 2 tag
+		nfc.RequestTagID(BrickletNFCRFID.TAG_TYPE_TYPE2);
+
+		System.Console.WriteLine("Press key to exit");
+		System.Console.ReadKey();
+		ipcon.Disconnect();
+    }
+}
