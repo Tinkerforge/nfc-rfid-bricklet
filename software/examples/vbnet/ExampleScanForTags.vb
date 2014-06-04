5b4c01e390fb9004a1d79ab9e99e29f271fffe85
Olaf Lüke <olaf@tinkerforge.com>
2014-06-04 19:21:31 +0200

    add VB.NET examples

---
+++ software/examples/vbnet/ExampleScanForTags.vb
@@ -0,0 +1,50 @@
+Imports Tinkerforge
+
+Module ExampleCallback
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "hjw" ' Change to your UID
+
+	Private tagType As Byte = 0
+
+    ' Callback function for state changed callback
+    Sub StateChangedCB(ByVal sender As BrickletNFCRFID, ByVal state As Byte, ByVal idle As Boolean)
+		If idle Then
+			tagType = (tagType + 1) Mod 3
+			sender.requestTagID(tagType)
+		End If
+
+		If state = BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY Then
+			Dim newTagType As Byte
+			Dim tidLength As Byte
+			Dim tid() As Byte
+
+			sender.GetTagID(newTagType, tidLength, tid)
+			If tidLength = 7 Then
+				System.Console.WriteLine("Fount tag of type {0} with ID [{1:X}, {2:X}, {3:X}, {4:X}, {5:X}, {6:X}, {7:X}]",
+										 newTagType, tid(0), tid(1), tid(2), tid(3), tid(4), tid(5), tid(6))
+			Else
+				System.Console.WriteLine("Fount tag of type {0} with ID [{1:X}, {2:X}, {3:X}, {4:X}]",
+										 newTagType, tid(0), tid(1), tid(2), tid(3))
+			End If
+		End If
+
+    End Sub
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim nfc As New BrickletNFCRFID(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Register state changed callback to function StateChangedCB
+        AddHandler nfc.StateChanged, AddressOf StateChangedCB
+
+		nfc.RequestTagID(BrickletNFCRFID.TAG_TYPE_MIFARE_CLASSIC)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
---
+++ software/examples/vbnet/ExampleWriteReadType2.vb
@@ -0,0 +1,54 @@
+Imports Tinkerforge
+
+Module ExampleCallback
+    Const HOST As String = "localhost"
+    Const PORT As Integer = 4223
+    Const UID As String = "hjw" ' Change to your UID
+
+    ' Callback function for state changed callback
+    Sub StateChangedCB(ByVal sender As BrickletNFCRFID, ByVal state As Byte, ByVal idle As Boolean)
+		If state = BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY Then
+			System.Console.WriteLine("Tag found")
+
+			' Write 16 byte to pages 5-8
+			Dim dataWrite() As Byte = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+			sender.WritePage(5, dataWrite)
+			System.Console.WriteLine("Writing data...")
+		ElseIf state = BrickletNFCRFID.STATE_WRITE_PAGE_READY Then
+			' Request pages 5-8
+			sender.RequestPage(5)
+			System.Console.WriteLine("Requesting data...")
+		ElseIf state = BrickletNFCRFID.STATE_REQUEST_PAGE_READY Then
+			' Get and print pages
+
+			Dim data As Byte() = sender.GetPage()
+			Dim str As String = "" & data(0).ToString()
+			For i As Integer = 0 To data.Length-1
+				str = str & ", " & data(i).ToString()
+			Next
+			System.Console.WriteLine("Read data: [{0}]", str)
+		ElseIf (state And (1 << 6)) = (1 << 6) Then
+			' All errors have bit 6 set
+			System.Console.WriteLine("Error: {0}", state)
+		End If
+
+    End Sub
+
+    Sub Main()
+        Dim ipcon As New IPConnection() ' Create IP connection
+        Dim nfc As New BrickletNFCRFID(UID, ipcon) ' Create device object
+
+        ipcon.Connect(HOST, PORT) ' Connect to brickd
+        ' Don't use device before ipcon is connected
+
+        ' Register state changed callback to function StateChangedCB
+        AddHandler nfc.StateChanged, AddressOf StateChangedCB
+
+		' Select NFC Forum Type 2 tag
+		nfc.RequestTagID(BrickletNFCRFID.TAG_TYPE_TYPE2)
+
+        System.Console.WriteLine("Press key to exit")
+        System.Console.ReadKey()
+        ipcon.Disconnect()
+    End Sub
+End Module
