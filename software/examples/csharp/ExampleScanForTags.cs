fe4511229fa1fa9db9be9957cdeb619ed4a3c794
Matthias Bolte <matthias@tinkerforge.com>
2014-08-06 15:46:01 +0200

    Fix typos

--- software/examples/csharp/ExampleScanForTags.cs
+++ software/examples/csharp/ExampleScanForTags.cs
@@ -27,7 +27,8 @@ class Example
 
 			if(tidLength == 7)
 			{
-				System.Console.WriteLine("Found tag of type {0} with ID [{1:X} {2:X} {3:X} {4:X} {5:X} {6:X} {7:X}]",
+				System.Console.WriteLine("Found tag of type {0} with ID " +
+				                         "[{1:X} {2:X} {3:X} {4:X} {5:X} {6:X} {7:X}]",
 				                         tagType, tid[0], tid[1], tid[2], tid[3], tid[4], tid[5], tid[6]);
 			}
 			else
