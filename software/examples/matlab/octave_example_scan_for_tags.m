acc4c9f1c611980b9ce0d7984dd88ed39e625146
Matthias Bolte <matthias@tinkerforge.com>
2015-06-29 12:47:47 +0200

    octave: Handle short difference between Octave 3.6 and 3.8
    In Octave 3.6 a short is not automatically handled as a number.

--- software/examples/matlab/octave_example_scan_for_tags.m
+++ software/examples/matlab/octave_example_scan_for_tags.m
@@ -4,23 +4,23 @@ function octave_example_scan_for_tags()
     global nfc;
     global tag_type;
 
-    HOST = 'localhost';
+    HOST = "localhost";
     PORT = 4223;
-    UID = 'hjw'; % Change to your UID
-    
+    UID = "fobar"; % Change to your UID
+
     ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
     nfc = java_new("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
     tag_type = 0;
 
     ipcon.connect(HOST, PORT); % Connect to brickd
-    % Don't use device before ipcon is connected
+    % Don"t use device before ipcon is connected
 
     % Register state changed callback to function cb_state_changed
     nfc.addStateChangedCallback(@cb_state_changed);
-    
+
     nfc.requestTagID(nfc.TAG_TYPE_MIFARE_CLASSIC);
 
-    input('Press any key to exit...\n', 's');
+    input("Press any key to exit...\n", "s");
     ipcon.disconnect();
 end
 
@@ -28,23 +28,34 @@ end
 function cb_state_changed(e)
     global tag_type
     global nfc
-    
+
     % Cycle through all types
     if e.idle
         tag_type = mod((tag_type + 1), 3);
         nfc.requestTagID(tag_type);
     end
-    
-    if e.state == nfc.STATE_REQUEST_TAG_ID_READY
-        ret = nfc.getTagID();
 
-        if ret.tidLength == 4
-            fprintf('Found tag of type %d with ID [%x %x %x %x]\n', ...
-                    ret.tagType, ret.tid(1), ret.tid(2), ret.tid(3), ret.tid(4));
+    if short2int(e.state) == short2int(nfc.STATE_REQUEST_TAG_ID_READY)
+        ret = nfc.getTagID();
+        tid = ret.tid;
+
+        if short2int(ret.tidLength) == 4
+            fprintf("Found tag of type %d with ID [%02x %02x %02x %02x]\n", ...
+                    short2int(ret.tagType), short2int(tid(1)), short2int(tid(2)), ...
+                    short2int(tid(3)), short2int(tid(4)));
         else
-            fprintf('Found tag of type %d with ID [%x %x %x %x %x %x %x]\n', ...
-                    ret.tagType, ret.tid(1), ret.tid(2), ret.tid(3), ret.tid(4), ...
-                    ret.tid(5), ret.tid(6), ret.tid(7));
+            fprintf("Found tag of type %d with ID [%02x %02x %02x %02x %02x %02x %02x]\n", ...
+                    short2int(ret.tagType), short2int(tid(1)), short2int(tid(2)), ...
+                    short2int(tid(3)), short2int(tid(4)), short2int(tid(5)), ...
+                    short2int(tid(6)), short2int(tid(7)));
         end
+    end
+end
+
+function int = short2int(short)
+    if compare_versions(version(), "3.8", "<=")
+        int = short.intValue();
+    else
+        int = short;
     end
 end
--- software/examples/matlab/octave_example_write_read_type2.m
+++ software/examples/matlab/octave_example_write_read_type2.m
@@ -1,52 +1,61 @@
-function matlab_example_write_read_type2()
+function octave_example_write_read_type2()
     more off;
 
     global nfc;
 
-    HOST = 'localhost';
+    HOST = "localhost";
     PORT = 4223;
-    UID = 'hjw'; % Change to your UID
-    
+    UID = "fobar"; % Change to your UID
+
     ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
     nfc = java_new("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
-    
-    ipcon = IPConnection(); % Create IP connection
-    nfc = BrickletNFCRFID(UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
-    % Don't use device before ipcon is connected
+    % Don"t use device before ipcon is connected
 
     % Register state changed callback to function cb_state_changed
     nfc.addStateChangedCallback(@cb_state_changed);
-    
+
     % Select NFC Forum Type 2 tag
     nfc.requestTagID(nfc.TAG_TYPE_TYPE2);
 
-    input('Press any key to exit...\n', 's');
+    input("Press any key to exit...\n", "s");
     ipcon.disconnect();
 end
 
 % Callback function for state changed
 function cb_state_changed(e)
     global nfc
-    
-    if e.state == nfc.STATE_REQUEST_TAG_ID_READY
-        fprintf('Tag found\n');
-        
+
+    if short2int(e.state) == short2int(nfc.STATE_REQUEST_TAG_ID_READY)
+        fprintf("Tag found\n");
+
         % Write 16 byte to pages 5-8
         data_write = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         nfc.writePage(5, data_write);
-        fprintf('Writing data...\n');
-    elseif e.state == nfc.STATE_WRITE_PAGE_READY
+        fprintf("Writing data...\n");
+    elseif short2int(e.state) == short2int(nfc.STATE_WRITE_PAGE_READY)
         % Request pages 5-8
         nfc.requestPage(5);
-        fprintf('Requesting data...\n');
-    elseif e.state == nfc.STATE_REQUEST_PAGE_READY
+        fprintf("Requesting data...\n");
+    elseif short2int(e.state) == short2int(nfc.STATE_REQUEST_PAGE_READY)
         % Get and print pages
         data = nfc.getPage();
-        fprintf('Read data: [%d]\n', data);
-    elseif (bitand(e.state, 64) == 64)
+        fprintf("Read data: [%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x]\n",
+                short2int(data(1)), short2int(data(2)), short2int(data(3)), short2int(data(4)),
+                short2int(data(5)), short2int(data(6)), short2int(data(7)), short2int(data(8)),
+                short2int(data(9)), short2int(data(10)), short2int(data(11)), short2int(data(12)),
+                short2int(data(13)), short2int(data(14)), short2int(data(15)), short2int(data(16)));
+    elseif (bitand(short2int(e.state), 64) == 64)
         % All errors have bit 6 set
-        fprintf('Error: %d\n', e.state);
+        fprintf("Error: %d\n", short2int(e.state));
+    end
+end
+
+function int = short2int(short)
+    if compare_versions(version(), "3.8", "<=")
+        int = short.intValue();
+    else
+        int = short;
     end
 end
