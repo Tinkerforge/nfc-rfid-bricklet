d88d8d79f0ad50a952c272ec705e43cff5ec6164
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:06:30 +0200

    Cleanup Perl examples

--- software/examples/perl/example_scan_for_tags.pl
+++ software/examples/perl/example_scan_for_tags.pl
@@ -1,14 +1,14 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletNFCRFID;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'hjw'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-our $nfc = Tinkerforge::BrickletNFCRFID->new(&UID, $ipcon); # Create device object
+our $nr = Tinkerforge::BrickletNFCRFID->new(&UID, $ipcon); # Create device object
 our $current_tag_type = 0;
 
 # Callback function for state changed callback
@@ -18,11 +18,11 @@ sub cb_state_changed
 
 	if($idle) {
 		$current_tag_type = ($current_tag_type + 1) % 3;
-		$nfc->request_tag_id($current_tag_type);
+		$nr->request_tag_id($current_tag_type);
 	}
 
-	if($state == $nfc->STATE_REQUEST_TAG_ID_READY) {
-		my ($tag_type, $tid_length, $tid) = $nfc->get_tag_id();
+	if($state == $nr->STATE_REQUEST_TAG_ID_READY) {
+		my ($tag_type, $tid_length, $tid) = $nr->get_tag_id();
 		my $s = "Found tag of type " . $tag_type . " with ID [" . sprintf("%x", @{$tid}[0]);
 
 		for(my $i = 1; $i < $tid_length; $i++) {
@@ -39,9 +39,9 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Register state changed callback to function cb_state_changed
-$nfc->register_callback($nfc->CALLBACK_STATE_CHANGED, 'cb_state_changed');
+$nr->register_callback($nr->CALLBACK_STATE_CHANGED, 'cb_state_changed');
 
-$nfc->request_tag_id($nfc->TAG_TYPE_MIFARE_CLASSIC);
+$nr->request_tag_id($nr->TAG_TYPE_MIFARE_CLASSIC);
 
 print "Press any key to exit...\n";
 <STDIN>;
--- software/examples/perl/example_write_read_type2.pl
+++ software/examples/perl/example_write_read_type2.pl
@@ -1,14 +1,14 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletNFCRFID;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'hjw'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-our $nfc = Tinkerforge::BrickletNFCRFID->new(&UID, $ipcon); # Create device object
+our $nr = Tinkerforge::BrickletNFCRFID->new(&UID, $ipcon); # Create device object
 
 # Callback function for state changed callback
 sub cb_state_changed
@@ -21,15 +21,15 @@ sub cb_state_changed
 		# Write 16 byte to pages 5-8
 		my @data_write = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-		$nfc->write_page(5, @data_write);
+		$nr->write_page(5, @data_write);
 		print "Writing data...\n";
 	} elsif($state == Tinkerforge::BrickletNFCRFID->STATE_WRITE_PAGE_READY) {
 		# Request pages 5-8
-		$nfc->request_page(5);
+		$nr->request_page(5);
 		print "Requesting data...\n";
 	} elsif($state == Tinkerforge::BrickletNFCRFID->STATE_REQUEST_PAGE_READY) {
 		# Get and print pages
-		my @data = $nfc->get_page();
+		my @data = $nr->get_page();
 		my $s = "Read data: [" . $data[0];
 
 		for(my $i = 1; $i < scalar @data; $i++) {
@@ -49,10 +49,10 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Register state changed callback to function cb_state_changed
-$nfc->register_callback($nfc->CALLBACK_STATE_CHANGED, 'cb_state_changed');
+$nr->register_callback($nr->CALLBACK_STATE_CHANGED, 'cb_state_changed');
 
 # Select NFC Forum Type 2 tag
-$nfc->request_tag_id($nfc->TAG_TYPE_TYPE2);
+$nr->request_tag_id($nr->TAG_TYPE_TYPE2);
 
 print "Press any key to exit...\n";
 <STDIN>;
