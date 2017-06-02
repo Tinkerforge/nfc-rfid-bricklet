7069dffa421d186afc92843eeaa6585a82d3660d
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:20:11 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_scan_for_tags.m
+++ software/examples/matlab/octave_example_scan_for_tags.m
@@ -8,8 +8,8 @@ function octave_example_scan_for_tags()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your NFC/RFID Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    nr = java_new("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    nr = javaObject("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
     tag_type = 0;
 
     ipcon.connect(HOST, PORT); % Connect to brickd
--- software/examples/matlab/octave_example_write_read_type2.m
+++ software/examples/matlab/octave_example_write_read_type2.m
@@ -7,8 +7,8 @@ function octave_example_write_read_type2()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your NFC/RFID Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    nr = java_new("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    nr = javaObject("com.tinkerforge.BrickletNFCRFID", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
