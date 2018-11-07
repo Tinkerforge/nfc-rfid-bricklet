38cb77d43500d3d787812a78a3c2401bfa74dec4
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:25:03 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_scan_for_tags.rs
+++ software/examples/rust/example_scan_for_tags.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, nfc_rfid_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, nfc_rfid_bricklet::*};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -7,33 +7,33 @@ const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let nfc_rfid_bricklet = NFCRFIDBricklet::new(UID, &ipcon); // Create device object
+    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
-    //Create listener for state changed events.
-    let state_changed_listener = nfc_rfid_bricklet.get_state_changed_receiver();
-    // Spawn thread to handle received events. This thread ends when the nfc_rfid_bricklet
+    //Create receiver for state changed events.
+    let state_changed_receiver = nr.get_state_changed_receiver();
+    // Spawn thread to handle received events. This thread ends when the nr
     // is dropped, so there is no need for manual cleanup.
-    let nfc_rfid_bricklet_copy = nfc_rfid_bricklet.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
+    let nr_copy = nr.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
         let mut current_tag_type = NFC_RFID_BRICKLET_TAG_TYPE_MIFARE_CLASSIC;
-        for event in state_changed_listener {
-            if event.idle {
+        for state_change in state_changed_receiver {
+            if state_change.idle {
                 current_tag_type = (current_tag_type + 1) % 3;
-                nfc_rfid_bricklet_copy.request_tag_id(current_tag_type);
+                nr_copy.request_tag_id(current_tag_type);
             }
 
-            if event.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
-                let tag_id = nfc_rfid_bricklet_copy.get_tag_id().recv().unwrap();
+            if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
+                let tag_id = nr_copy.get_tag_id().recv().unwrap();
                 println!("Found tag of type {} with ID {:x?}", tag_id.tag_type, tag_id.tid[0..tag_id.tid_length as usize].to_vec());
             }
         }
     });
 
     // Start scan loop
-    nfc_rfid_bricklet.request_tag_id(NFC_RFID_BRICKLET_TAG_TYPE_MIFARE_CLASSIC);
+    nr.request_tag_id(NFC_RFID_BRICKLET_TAG_TYPE_MIFARE_CLASSIC);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_write_read_type2.rs
+++ software/examples/rust/example_write_read_type2.rs
@@ -1,5 +1,5 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, nfc_rfid_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, nfc_rfid_bricklet::*};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -7,44 +7,44 @@ const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let nfc_rfid_bricklet = NFCRFIDBricklet::new(UID, &ipcon); // Create device object
+    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
-    //Create listener for state changed events.
-    let state_changed_listener = nfc_rfid_bricklet.get_state_changed_receiver();
-    // Spawn thread to handle received events. This thread ends when the nfc_rfid_bricklet
+    //Create receiver for state changed events.
+    let state_changed_receiver = nr.get_state_changed_receiver();
+    // Spawn thread to handle received events. This thread ends when the nr
     // is dropped, so there is no need for manual cleanup.
-    let nfc_rfid_bricklet_copy = nfc_rfid_bricklet.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
+    let nr_copy = nr.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
-        for event in state_changed_listener {
-            if event.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
+        for state_change in state_changed_receiver {
+            if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
                 println!("Tag found");
 
                 // Write 16 byte to pages 5-8
                 let data_write = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-                nfc_rfid_bricklet_copy.write_page(5, data_write);
+                nr_copy.write_page(5, data_write);
                 println!("Writing data...");
-            } else if event.state == NFC_RFID_BRICKLET_STATE_WRITE_PAGE_READY {
+            } else if state_change.state == NFC_RFID_BRICKLET_STATE_WRITE_PAGE_READY {
                 // Request pages 5-8
-                nfc_rfid_bricklet_copy.request_page(5);
+                nr_copy.request_page(5);
                 println!("Requesting data...");
-            } else if event.state == NFC_RFID_BRICKLET_STATE_REQUEST_PAGE_READY {
+            } else if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_PAGE_READY {
                 // Get and print pages
-                let data = nfc_rfid_bricklet_copy.get_page().recv().unwrap();
+                let data = nr_copy.get_page().recv().unwrap();
 
                 println!("Read data: {:x?}", data);
-            } else if (event.state & (1 << 6)) == (1 << 6) {
+            } else if (state_change.state & (1 << 6)) == (1 << 6) {
                 // All errors have bit 6 set
-                println!("Error: {}", event.state);
+                println!("Error: {}", state_change.state);
             }
         }
     });
 
     // Select NFC Forum Type 2 tag
-    nfc_rfid_bricklet.request_tag_id(NFC_RFID_BRICKLET_TAG_TYPE_TYPE2);
+    nr.request_tag_id(NFC_RFID_BRICKLET_TAG_TYPE_TYPE2);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
