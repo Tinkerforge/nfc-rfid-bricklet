bbaeb96252df725a779bbacfdfe711a525fb1d4b
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:58:18 +0100

    go: Change import path.

--- software/examples/go/example_scan_for_tags.go
+++ software/examples/go/example_scan_for_tags.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-"tinkerforge/ipconnection"
-    "tinkerforge/nfc_rfid_bricklet"
+    "github.com/tinkerforge/go-api-bindings/ipconnection"
+    "github.com/tinkerforge/go-api-bindings/nfc_rfid_bricklet"
 )
 
 const ADDR string = "localhost:4223"
