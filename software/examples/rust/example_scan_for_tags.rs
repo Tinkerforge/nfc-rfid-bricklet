266ca2bbbfc6b7d3efb72ed41ff939b4f59fb3fa
Matthias Bolte <matthias@tinkerforge.com>
2020-03-18 15:40:41 +0100

    rust: Update to new skeleton

--- software/examples/rust/example_scan_for_tags.rs
+++ software/examples/rust/example_scan_for_tags.rs
@@ -1,32 +1,32 @@
 use std::{error::Error, io, thread};
 use tinkerforge::{ip_connection::IpConnection, nfc_rfid_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     let state_changed_receiver = nr.get_state_changed_callback_receiver();
 
-    // Spawn thread to handle received events.
+    // Spawn thread to handle received callback messages.
     // This thread ends when the `nr` object
     // is dropped, so there is no need for manual cleanup.
     let nr_copy = nr.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
         let mut current_tag_type = NFC_RFID_BRICKLET_TAG_TYPE_MIFARE_CLASSIC;
-        for state_change in state_changed_receiver {
-            if state_change.idle {
+        for state_changed in state_changed_receiver {
+            if state_changed.idle {
                 current_tag_type = (current_tag_type + 1) % 3;
                 nr_copy.request_tag_id(current_tag_type);
             }
 
-            if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
+            if state_changed.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
                 let tag_id = nr_copy.get_tag_id().recv().unwrap();
                 println!("Found tag of type {} with ID {:x?}", tag_id.tag_type, tag_id.tid[0..tag_id.tid_length as usize].to_vec());
             }
--- software/examples/rust/example_write_read_type2.rs
+++ software/examples/rust/example_write_read_type2.rs
@@ -1,26 +1,26 @@
 use std::{error::Error, io, thread};
 use tinkerforge::{ip_connection::IpConnection, nfc_rfid_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your NFC/RFID Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let nr = NfcRfidBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     let state_changed_receiver = nr.get_state_changed_callback_receiver();
 
-    // Spawn thread to handle received events.
+    // Spawn thread to handle received callback messages.
     // This thread ends when the `nr` object
     // is dropped, so there is no need for manual cleanup.
     let nr_copy = nr.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
-        for state_change in state_changed_receiver {
-            if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
+        for state_changed in state_changed_receiver {
+            if state_changed.state == NFC_RFID_BRICKLET_STATE_REQUEST_TAG_ID_READY {
                 println!("Tag found");
 
                 // Write 16 byte to pages 5-8
@@ -28,18 +28,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                 nr_copy.write_page(5, data_write);
                 println!("Writing data...");
-            } else if state_change.state == NFC_RFID_BRICKLET_STATE_WRITE_PAGE_READY {
+            } else if state_changed.state == NFC_RFID_BRICKLET_STATE_WRITE_PAGE_READY {
                 // Request pages 5-8
                 nr_copy.request_page(5);
                 println!("Requesting data...");
-            } else if state_change.state == NFC_RFID_BRICKLET_STATE_REQUEST_PAGE_READY {
+            } else if state_changed.state == NFC_RFID_BRICKLET_STATE_REQUEST_PAGE_READY {
                 // Get and print pages
                 let data = nr_copy.get_page().recv().unwrap();
 
                 println!("Read data: {:x?}", data);
-            } else if (state_change.state & (1 << 6)) == (1 << 6) {
+            } else if (state_changed.state & (1 << 6)) == (1 << 6) {
                 // All errors have bit 6 set
-                println!("Error: {}", state_change.state);
+                println!("Error: {}", state_changed.state);
             }
         }
     });
